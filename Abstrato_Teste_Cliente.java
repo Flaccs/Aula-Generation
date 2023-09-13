03d5ddedb517af99d45e7482079176dd6c689b87
Flaccs <143420269+Flaccs@users.noreply.github.com>
2023-09-13 14:18:49 -0300

    Create Abstrato_Teste_Cliente.java

---
+++ Abstrato_Teste_Cliente.java
@@ -0,0 +1,28 @@
+package abstrata;
+
+public class Teste_Cliente extends Pessoa_Juridica{
+
+	public static void main(String[] args) {
+		
+		
+		Pessoa_Juridica j1 = new Pessoa_Juridica();
+		Pessoa_Fisica f1 = new Pessoa_Fisica();
+		Pessoa_Juridica j2 = new Pessoa_Juridica();
+		Pessoa_Fisica f2 = new Pessoa_Fisica();
+		
+		f1.nome();
+		j1.nome();
+		f2.sobrenome();
+		j2.sobrenome();
+		
+	
+		
+	
+		
+		
+		
+
+
+	}
+
+}
