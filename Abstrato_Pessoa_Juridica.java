2ff40b1c520133a9f31c9d7534c0d5cb22e1bf59
Flaccs <143420269+Flaccs@users.noreply.github.com>
2023-09-13 14:18:05 -0300

    Create Abstrato_Pessoa_Juridica.java

---
+++ Abstrato_Pessoa_Juridica.java
@@ -0,0 +1,15 @@
+package abstrata;
+
+public class Pessoa_Juridica extends Cliente{
+
+	
+		@Override
+		public void nome() {
+		System.out.println("Nome Composto.");
+		}
+		@Override 
+		public void sobrenome() {
+			System.out.println("Sobrenome composto.");
+
+}
+}
