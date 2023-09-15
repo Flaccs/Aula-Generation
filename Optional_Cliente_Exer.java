e98d03da88aaff38d707083dd9d309cb43d29d04
Flaccs <143420269+Flaccs@users.noreply.github.com>
2023-09-15 15:37:26 -0300

    Create Optional_Cliente_Exer.java

---
+++ Optional_Cliente_Exer.java
@@ -0,0 +1,84 @@
+package poo;
+
+
+
+public class Cliente {
+
+	public static void main(String[] args) {
+
+	}
+		private String nome;
+		private String sobrenome;
+		private int idade;
+		//private int peso;
+		//private float altura;
+		
+	
+		public Cliente(String nome, String sobrenome, int idade) {
+		this.nome = nome;
+		this.sobrenome = sobrenome;
+		this.idade = idade;
+		//this.peso = peso;
+		//this.altura = altura;
+		
+		
+		
+} 
+		public Cliente(String nome2) {
+			// TODO Auto-generated constructor stub
+		}
+		public Cliente(String string, String string2, int i) {
+			// TODO Auto-generated constructor stub
+		}
+		public String getNome() {
+		return nome;
+		}
+		 
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+		public String getSobrenome() {
+			return sobrenome;
+		}
+		public void setSobrenome(String sobrenome) {
+			this.sobrenome = sobrenome;
+		}	
+		public int getIdade() {
+			return idade;
+		}
+		public void setIdade(int idade) {
+			this.idade = idade;	
+		}
+		/*public float getPeso() {
+			//return peso;
+		}
+		//public void setPeso(int peso) {
+			this.peso = peso;
+		}	
+		public float getAltura() {
+			return altura;
+		}
+		public void setAltura(float altura) {
+			this.altura = altura;
+		}	
+		*/
+	
+			
+				//public void visualizar() {
+				//System.out.println("********************Cliente********************");
+				//System.out.println("Nome: " + this.nome);
+			  //  System.out.println("Sobrenome: " + this.sobrenome);
+			   // System.out.println("Idade: " + this.idade);
+			    //System.out.println("Peso: " + this.peso);
+			   // System.out.println("Altura: " + this.altura);
+				//System.out.println("***********************************************");
+
+
+				
+		
+	
+			
+		
+		
+}
+		
